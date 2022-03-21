b0a291bc72294226106c6f65ff74e0536f7539ae
pung8146 <pung8146@naver.com>
2022-03-21 15:18:23 +0900

    다이어리리스트 최신순 정렬까지

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useReducer } from "react";
+import React, { useReducer, useRef } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./Routes/Components/Header";
 import Versus from "./Routes/Versus";
@@ -18,7 +18,7 @@ import MyHeader from "./Routes/Components/MyHeader";
 
 // import RouteTest from "./Routes/Components/RouteTest";
 
-const reducer = (state, action) => {
+const reducer = (state: any, action: any) => {
   let newState = [];
   switch (action.type) {
     case " INIT": {
@@ -31,38 +31,117 @@ const reducer = (state, action) => {
       newState = [newItem, ...state];
       break;
     }
-    case 'REMOVE' : {
-      newState = state.filter((it)=>it.id !== action.targetId);
+    case "REMOVE": {
+      newState = state.filter((it: any) => it.id !== action.targetId);
       break;
     }
-  }
-    case 'EDIT' : {
-      newState = state.map((it)=>it.id === action.data.id? {...aciton.data}: it)
+    case "EDIT": {
+      newState = state.map((it: any) =>
+        it.id === action.data.id ? { ...action.data } : it
+      );
       break;
     }
     default:
       return state;
   }
+  return newState;
 };
 
+export const DiaryStateContext = React.createContext("");
+export const DiaryDispatchContext = React.createContext({});
+
+const dummyData = [
+  {
+    id: 1,
+    emotion: 1,
+    content: "오늘의 일기 1번",
+    date: 1647833955865,
+  },
+  {
+    id: 2,
+    emotion: 2,
+    content: "오늘의 일기 2번",
+    date: 1647833955866,
+  },
+  {
+    id: 3,
+    emotion: 3,
+    content: "오늘의 일기 3번",
+    date: 1647833955867,
+  },
+  {
+    id: 4,
+    emotion: 4,
+    content: "오늘의 일기 4번",
+    date: 1647833955868,
+  },
+  {
+    id: 5,
+    emotion: 5,
+    content: "오늘의 일기 5번",
+    date: 1647833955869,
+  },
+];
+
 function App() {
-  const [data, dispatch] = useReducer(reducer, []);
+  const [data, dispatch] = useReducer(reducer, dummyData);
+
+  const dataId = useRef(0);
+  // Create
+  const onCreate = (date: any, content: any, emotion: any) => {
+    dispatch({
+      type: "CREATE",
+      data: {
+        id: dataId.current,
+        date: new Date(date).getTime(),
+        content,
+        emotion,
+      },
+    });
+    dataId.current += 1;
+  };
+  // Remove
+  const onRemove = (targetId: any) => {
+    dispatch({ tpye: "REMOVE", targetId });
+  };
+  // EDIT
+  const onEdit = (targetId: any, date: any, content: any, emotion: any) => {
+    dispatch({
+      type: "EDIT",
+      data: {
+        id: targetId,
+        date: new Date(date).getTime(),
+        content,
+        emotion,
+      },
+    });
+  };
 
   return (
-    <BrowserRouter>
-      {/* <Header /> */}
-      {/* <Loading /> */}
-      <div className="App">
-        <Routes>
-          {/* <Route path="/" element={<Versus />} />  */}
-          <Route path="/" element={<Home />} />
-          <Route path="/blog" element={<Blog />} />
-          <Route path="/new" element={<New />} />
-          <Route path="/edit" element={<Edit />} />
-          <Route path="/diary/:id" element={<Diary />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <DiaryStateContext.Provider value={data}>
+      <DiaryDispatchContext.Provider
+        value={{
+          onCreate,
+          onEdit,
+          onRemove,
+        }}
+      >
+        <BrowserRouter>
+          {/* <Header /> */}
+          {/* <Loading /> */}
+          <div className="App">
+            <Routes>
+              {/* <Route path="/" element={<Versus />} />  */}
+              <Route path="/" element={<Home />} />
+              <Route path="/blog" element={<Blog />} />
+              <Route path="/new" element={<New />} />
+              <Route path="/edit" element={<Edit />} />
+              <Route path="/diary/:id" element={<Diary />} />
+            </Routes>
+          </div>
+        </BrowserRouter>
+      </DiaryDispatchContext.Provider>
+    </DiaryStateContext.Provider>
   );
 }
 
--- src/Routes/pages/Home.tsx
+++ src/Routes/pages/Home.tsx
@@ -1,25 +1,62 @@
-import styled from "styled-components";
+import { useContext, useEffect, useState } from "react";
+import MyHeader from "../Components/MyHeader";
+import MyButton from "../Components/MyButton";
+import DiaryList from "../Components/DiaryList";
+import { DiaryStateContext } from "../../App";
 
-const Title = styled.h1`
-    color:${(props) => props.theme.textColor};
-`
+const Home = () => {
+  const diaryList: any = useContext(DiaryStateContext);
 
-const Wrapper = styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    width:100vw;
-    background-color: ${(props) => props.theme.backgroundColor};
-    font-family: "Nanum pen Script";
-`
+  const [data, setData] = useState([]);
+  const [curDate, setCurDate] = useState(new Date());
+  const headText = `${curDate.getFullYear()}년 ${curDate.getMonth() + 1}월`;
 
-function Home() {
-    return (
-        <Wrapper> 
-            <Title>Hi</Title>
-            <h2> 이곳은 홈 입니다</h2>
-        </Wrapper>
-    )
-}
+  useEffect(() => {
+    if (diaryList.length >= 1) {
+      const firstDay = new Date(
+        curDate.getFullYear(),
+        curDate.getMonth(),
+        1
+      ).getTime();
+
+      const lastDay = new Date(
+        curDate.getFullYear(),
+        curDate.getMonth() + 1,
+        0
+      ).getTime();
+
+      setData(
+        diaryList.filter((it: any) => firstDay <= it.date && it.date <= lastDay)
+      );
+    }
+  }, [diaryList, curDate]);
+
+  useEffect(() => {
+    console.log(data);
+  }, [data]);
+
+  const increaseMonth = () => {
+    setCurDate(
+      new Date(curDate.getFullYear(), curDate.getMonth() + 1, curDate.getDate())
+    );
+  };
+
+  const decreaseMonth = () => {
+    setCurDate(
+      new Date(curDate.getFullYear(), curDate.getMonth() - 1, curDate.getDate())
+    );
+  };
+
+  return (
+    <div>
+      <MyHeader
+        headText={headText}
+        leftChild={<MyButton text={"<"} onClick={decreaseMonth} />}
+        rightChild={<MyButton text={">"} onClick={increaseMonth} />}
+      />
+      <DiaryList diaryList={data} />
+    </div>
+  );
+};
 
 export default Home;
